4134ea98e21f5bf74c90a9bf92f368d82a95c776
Danilo Sousa <code@danilosousa.net>
2013-10-25 11:12:04 -0200

    make sure the secret key comes from environment variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-MalvadosFeed::Application.config.secret_key_base = ENV['secret_key_base'] || '414cbbc9e2f1b82246a1e1ab99b359be3768efc1fb6955a9eab99338a350a4a9b1d4d69f1829cbc377441d7422f9bc31814c5d6d1b9d9ad30a8889912d170102'
+MalvadosFeed::Application.config.secret_key_base = ENV['secret_key_base']
