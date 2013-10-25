3c8bba142ee28932e327c0d9a8262b4aff426d1d
Danilo Sousa <code@danilosousa.net>
2013-10-25 14:38:53 -0200

    set a default secret_key_base value
    again!

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-MalvadosFeed::Application.config.secret_key_base = ENV['secret_key_base']
+MalvadosFeed::Application.config.secret_key_base = ENV['secret_key_base'] || 'empty'
