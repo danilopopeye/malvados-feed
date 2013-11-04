deebe082cd818f7c5af68ed39a053d7146d47cd7
Danilo Sousa <code@danilosousa.net>
2013-11-03 22:28:29 -0200

    scrapper only need to handle #latest and #all
    all the old scrapper logic regard the fetch and build
    of the model will be on the StripBuilder class.

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -1,100 +1,38 @@
-require 'open-uri'
-require 'nokogiri'
-require 'yaml'
-
 class Scrapper
-  attr_reader :settings
+  attr_reader :builder
 
   def initialize
-    @settings = YAML.load_file Rails.root.join 'config/application.yml'
-    @settings.merge! @settings.fetch(Rails.env, {})
-    @settings.symbolize_keys!
+    @builder ||= StripBuilder.new
   end
+
+  def latest
+    scrap @builder.latest
+  end
+
+  def all
+    strip_number = @builder.latest
+
+    while strip_number > 0 do
+      scrap strip_number
+      strip_number = @builder.previous strip_number
+    end
+  end
+
+  private
 
   def scrap number
     print "Started #{number}: "
 
-    if exists? number
+    if @builder.exists? number
       print "SKIPPED\n"
     else
-      if get_and_parse(number)
+      strip = @builder.build(number)
+
+      if strip.save
         puts "DONE"
       else
         puts "FAIL"
       end
     end
-  end
-
-  def latest
-    strip = get_latest
-    scrap strip
-  end
-
-  def all
-    strip_number = get_latest
-
-    while strip_number > 0 do
-      scrap strip_number
-      strip_number = get_previous strip_number
-    end
-  end
-
-  def url
-    @settings[:base_url]
-  end
-
-  def get_previous number
-    Strip.find_by(number: number)[:previous]
-  end
-
-  def get_latest
-    doc = Nokogiri::HTML open self.url
-    link = doc.css('frame[name="mainFrame"]').first
-    get_number link.attr('src') if link
-  end
-
-  def exists? number
-    Strip.where(number: number).exists?
-  end
-
-  def get_number strip
-    strip.match(/index([0-9]+)/)[1].to_i
-  end
-
-  def build_strip_url number
-    "#{self.url}index#{number}." + (number == 123 ? 'htm' : 'html')
-  end
-
-  def build_strip_image url
-    if url[0..3] == 'http'
-      url
-    else
-      self.url + url
-    end
-  end
-
-  def get_and_parse number
-    strip = Strip.new number: number
-
-    open build_strip_url(number) do |req|
-      strip[:updated_at] = req.last_modified
-      @doc = Nokogiri::HTML(req)
-    end
-
-    strip[:image] = build_strip_image @doc.css('img[src^="tir"]').attr('src')
-
-    header = @doc.css('img[src^="logo"]')
-
-    unless header.empty?
-      strip[:header] = build_strip_image header.attr('src')
-    end
-
-    if number > 1
-      strip[:previous] = get_number @doc.css('a[href*=index]').last.attr('href').to_s
-    else
-      strip[:previous] = 0
-    end
-
-    strip.save
   end
 end
--- spec/lib/scrapper_spec.rb
+++ spec/lib/scrapper_spec.rb
@@ -2,121 +2,23 @@ require 'spec_helper'
 
 describe Scrapper do
   subject { described_class.new }
-  let!(:base_url){ subject.url }
 
-  its(:url){ should match %r{www.malvados.com.br} }
-
-  describe :exists? do
-    it 'should return true when exists a strip with this number' do
-      create :strip, number: 1
-      subject.exists?(1).should be_true
-    end
-
-    it 'should return false when do not exists a strip with this number' do
-      subject.exists?(1).should be_false
-    end
+  before do
+    StripBuilder.any_instance.stub({
+      latest: 666, previous: 0
+    })
   end
 
   describe :latest do
     it 'should call #scrap with latest strip' do
-      subject.stub get_latest: 666
       subject.should_receive(:scrap).with 666
       subject.latest
     end
   end
 
-  describe :get_number do
-    it 'should parse the number from strip link' do
-      subject.get_number('index666.html').should == 666
-    end
-  end
-
-  describe :get_latest do
-    before do
-      latest = File.new Rails.root.join 'spec/fixtures/latest.html'
-      stub_request(:get, 'www.malvados.com.br').to_return(body: latest, status: 200)
-    end
-
-    it 'should return the latest strip number' do
-      subject.get_latest.should == 666
-    end
-  end
-
-  describe :get_previous do
-    it 'should return the previous strip number' do
-      create :strip, number: 666
-      subject.get_previous(666).should == 665
-    end
-  end
-
-  describe :build_strip_url do
-    it 'should generate the correct strip url' do
-      subject.build_strip_url(666).should == base_url + 'index666.html'
-    end
-
-    it 'should generate the correct url for strip 123' do
-      subject.build_strip_url(123).should == base_url + 'index123.htm'
-    end
-  end
-
-  describe :build_strip_image do
-    let(:image){ 'image.ext' }
-
-    it 'should generate the correct url for relative path' do
-      subject.build_strip_image(image).should == base_url + image
-    end
-
-    it 'should return the same url when is a full url' do
-      host = 'http://host/image.ext'
-      subject.build_strip_image(host).should == host
-    end
-  end
-
-  describe :get_and_parse do
-    # FIXME: should change to subject but nedd before to have been called
-    let(:strip){ Strip.find_by number: number }
-    let(:modified){ Time.now.utc }
-    let(:number){ 666 }
-
-    before do
-      stub_strip number, 'index_with_header', modified
-      described_class.new.get_and_parse number
-    end
-
-    context "should create a strip with parsed content with" do
-      it(:number){ strip[:number].should == 666 }
-      it(:previous){ strip[:previous].should == 665 }
-      it(:updated_at){ strip[:updated_at].equal? modified }
-      it(:header){ strip[:header].should == "#{base_url}logo#{number}.gif" }
-      it(:image){ strip[:image].should == "#{base_url}tirinha#{number}.gif" }
-    end
-
-    context 'exeptions' do
-      let(:strip){ Strip.find_by number: 1 }
-
-      before do
-        stub_strip 1, 'index_without_header', modified
-        described_class.new.get_and_parse 1
-      end
-
-      it ':header should be nil when strip does not have one' do
-        strip.header.should be_nil
-      end
-
-      it 'should set :previous to zero for strip number 1' do
-        strip[:previous].should == 0
-      end
-    end
-  end
-
   describe :all do
-    before do
-      subject.stub get_latest: 1
-      subject.stub get_previous: 0
-    end
-
     it 'should parse the strips in sequence' do
-      subject.should_receive(:scrap).with 1
+      subject.should_receive(:scrap).with 666
       subject.all
     end
 
@@ -124,12 +26,11 @@ describe Scrapper do
       subject.should_receive(:scrap).once
       subject.all
     end
-  end
 
-  private
-
-  def stub_strip number, fixture, modified_date
-    latest = File.new Rails.root.join "spec/fixtures/#{fixture}.html"
-    stub_request(:get, "http://www.malvados.com.br/index#{number}.html").to_return(body: latest, status: 200, header: { last_modified: modified_date })
+    it 'should parse each strip only once' do
+      create :strip, number: 666
+      subject.all
+      Strip.any_instance.should_not_receive :save
+    end
   end
 end
