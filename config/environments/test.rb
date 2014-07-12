2528f6628b29a3108d4e95f5ab05a3c7d93c7721
Danilo Sousa <code@danilosousa.net>
2014-07-12 16:04:59 +0100

    configure Rails.cache for all environments

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  # Use a different cache store in test.
+  config.cache_store = :null_store
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
