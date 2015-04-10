b0d240f9536a3f2fb275dc8e2198cf1bf666e73f
Danilo Sousa <code@danilosousa.net>
2015-04-10 00:43:24 -0300

    production memcached never expires

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, { compress: true, expires_in: 1.year }
+  config.cache_store = :dalli_store, { compress: true }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
